10107bd64612b3f48c52422547e425c7c7e235d9
fbalakirev <fbalakirev@gmail.com>
2015-09-22 08:31:36 -0600

    added delay core - inserts empty beats to match signal paths timing

--- cores/axis_delay_v1_0/axis_delay.v
+++ cores/axis_delay_v1_0/axis_delay.v
@@ -1,12 +1,10 @@
 
 `timescale 1 ns / 1 ps
 
-module axis_packetizer #
+module axis_delay #
 (
   parameter integer AXIS_TDATA_WIDTH = 32,
-  parameter integer CNTR_WIDTH = 32,
-  parameter         CONTINUOUS = "FALSE",
-  parameter         NON_BLOCKING = "FALSE"
+  parameter integer CNTR_WIDTH = 32
 )
 (
   // System signals
@@ -23,14 +21,13 @@ module axis_packetizer #
   // Master side
   input  wire                        m_axis_tready,
   output wire [AXIS_TDATA_WIDTH-1:0] m_axis_tdata,
-  output wire                        m_axis_tvalid,
-  output wire                        m_axis_tlast
+  output wire                        m_axis_tvalid
 );
 
   reg [CNTR_WIDTH-1:0] int_cntr_reg, int_cntr_next;
-  reg int_enbl_reg, int_enbl_next;
+  reg int_enbl_reg, int_enbl_next, int_rst_reg;
 
-  wire int_comp_wire, int_tvalid_wire, int_tlast_wire;
+  wire int_comp_wire, int_tvalid_wire;
 
   always @(posedge aclk)
   begin
@@ -38,43 +35,20 @@ module axis_packetizer #
     begin
       int_cntr_reg <= {(CNTR_WIDTH){1'b0}};
       int_enbl_reg <= 1'b0;
+      int_rst_reg  <= 1'b0;
     end
     else
     begin
       int_cntr_reg <= int_cntr_next;
       int_enbl_reg <= int_enbl_next;
+      int_rst_reg  <= 1'b1;
     end
   end
 
   assign int_comp_wire = int_cntr_reg < cfg_data;
-  assign int_tvalid_wire = int_enbl_reg & s_axis_tvalid;
-  assign int_tlast_wire = ~int_comp_wire;
+  assign int_tvalid_wire = ( ~int_enbl_reg | s_axis_tvalid ) & int_rst_reg;
 
   generate
-    if(CONTINUOUS == "TRUE")
-    begin : CONTINUOUS
-      always @*
-      begin
-        int_cntr_next = int_cntr_reg;
-        int_enbl_next = int_enbl_reg;
-
-        if(~int_enbl_reg & int_comp_wire)
-        begin
-          int_enbl_next = 1'b1;
-        end
-
-        if(m_axis_tready & int_tvalid_wire & int_comp_wire)
-        begin
-          int_cntr_next = int_cntr_reg + 1'b1;
-        end
-
-        if(m_axis_tready & int_tvalid_wire & int_tlast_wire)
-        begin
-          int_cntr_next = {(CNTR_WIDTH){1'b0}};
-        end
-      end
-    end
-    else
     begin : STOP
       always @*
       begin
@@ -83,7 +57,7 @@ module axis_packetizer #
 
         if(~int_enbl_reg & int_comp_wire)
         begin
-          int_enbl_next = 1'b1;
+          int_enbl_next = 1'b0;
         end
 
         if(m_axis_tready & int_tvalid_wire & int_comp_wire)
@@ -91,20 +65,15 @@ module axis_packetizer #
           int_cntr_next = int_cntr_reg + 1'b1;
         end
 
-        if(m_axis_tready & int_tvalid_wire & int_tlast_wire)
+        if(m_axis_tready & int_tvalid_wire & ~int_comp_wire)
         begin
-          int_enbl_next = 1'b0;
+          int_enbl_next = 1'b1;
         end
       end
     end
   endgenerate
 
-  if(NON_BLOCKING == "TRUE")  
-    assign s_axis_tready = ~int_enbl_reg | m_axis_tready;
-  else 
-    assign s_axis_tready = int_enbl_reg & m_axis_tready;
+  assign s_axis_tready = int_enbl_reg & m_axis_tready;
   assign m_axis_tdata = s_axis_tdata;
   assign m_axis_tvalid = int_tvalid_wire;
-  assign m_axis_tlast = int_enbl_reg & int_tlast_wire;
-
 endmodule
