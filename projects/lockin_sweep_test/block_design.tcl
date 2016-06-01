d59315ced912515bbabb3af1c267b7e03afd40e9
Fedor Balakirev <fbalakirev@gmail.com>
2016-06-01 17:21:52 -0600

    Updated for 2016.1

--- projects/lockin_sweep_test/block_design.tcl
+++ projects/lockin_sweep_test/block_design.tcl
@@ -1,4 +1,4 @@
-#lockin_sweep
+#lockin_sweep_test
 
 # Create processing_system7
 cell xilinx.com:ip:processing_system7:5.5 ps_0 {
@@ -310,14 +310,11 @@ cell xilinx.com:ip:cmpy:6.0 mult_0 {
 }
 
 # Create clk_wiz
-cell xilinx.com:ip:clk_wiz:5.2 pll_0 {
-  PRIMITIVE PLL
+cell xilinx.com:ip:clk_wiz:5.3 pll_0 {
   PRIM_IN_FREQ.VALUE_SRC USER
   PRIM_IN_FREQ 125.0
   CLKOUT1_USED true
-  CLKOUT2_USED true
-  CLKOUT1_REQUESTED_OUT_FREQ 125.0
-  CLKOUT2_REQUESTED_OUT_FREQ 250.0
+  CLKOUT1_REQUESTED_OUT_FREQ 250.0
 } {
   clk_in1 adc_0/adc_clk
 }
@@ -330,14 +327,14 @@ cell xilinx.com:ip:axis_clock_converter:1.1 fifo_dac {
   S_AXIS bcast_DDS/M00_AXIS
   s_axis_aclk ps_0/FCLK_CLK0
   s_axis_aresetn rst_0/peripheral_aresetn
-  m_axis_aclk pll_0/clk_out1
+  m_axis_aclk adc_0/adc_clk
   m_axis_aresetn const_1/dout
 }
 
 # Create axis_red_pitaya_dac
 cell pavel-demin:user:axis_red_pitaya_dac:1.0 dac_0 {} {
-  aclk pll_0/clk_out1
-  ddr_clk pll_0/clk_out2
+  aclk adc_0/adc_clk
+  ddr_clk pll_0/clk_out1
   locked pll_0/locked
   S_AXIS fifo_dac/M_AXIS
   dac_clk dac_clk_o
