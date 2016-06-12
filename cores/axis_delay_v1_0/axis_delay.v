6347c153f29a755675544746c1158d84186d8d50
Fedor Balakirev <fbalakirev@gmail.com>
2016-06-11 20:04:16 -0600

    Redesigned delay line

--- cores/axis_delay_v1_0/axis_delay.v
+++ cores/axis_delay_v1_0/axis_delay.v
@@ -6,74 +6,70 @@ module axis_delay #
   parameter integer AXIS_TDATA_WIDTH = 32,
   parameter integer CNTR_WIDTH = 32
 )
+
 (
   // System signals
   input  wire                        aclk,
   input  wire                        aresetn,
 
   input  wire [CNTR_WIDTH-1:0]       cfg_data,
+  input  wire [CNTR_WIDTH-1:0]       axis_data_count,  
 
   // Slave side
   output wire                        s_axis_tready,
   input  wire [AXIS_TDATA_WIDTH-1:0] s_axis_tdata,
   input  wire                        s_axis_tvalid,
 
+
+
   // Master side
   input  wire                        m_axis_tready,
   output wire [AXIS_TDATA_WIDTH-1:0] m_axis_tdata,
-  output wire                        m_axis_tvalid
+  output wire                        m_axis_tvalid,
+
+  // Slave side
+  output wire                        s_axis_fifo_tready,
+  input  wire [AXIS_TDATA_WIDTH-1:0] s_axis_fifo_tdata,
+  input  wire                        s_axis_fifo_tvalid,
+
+  // Master side
+  input  wire                        m_axis_fifo_tready,
+  output wire [AXIS_TDATA_WIDTH-1:0] m_axis_fifo_tdata,
+  output wire                        m_axis_fifo_tvalid
 );
 
-  reg [CNTR_WIDTH-1:0] int_cntr_reg, int_cntr_next;
-  reg int_enbl_reg, int_enbl_next, int_rst_reg;
+  reg int_enbl_reg, int_enbl_next;
+  wire int_comp_wire;
 
-  wire int_comp_wire, int_tvalid_wire;
+  assign int_comp_wire = axis_data_count > cfg_data;
 
   always @(posedge aclk)
   begin
     if(~aresetn)
     begin
-      int_cntr_reg <= {(CNTR_WIDTH){1'b0}};
       int_enbl_reg <= 1'b0;
-      int_rst_reg  <= 1'b0;
     end
     else
     begin
-      int_cntr_reg <= int_cntr_next;
       int_enbl_reg <= int_enbl_next;
-      int_rst_reg  <= 1'b1;
     end
   end
 
-  assign int_comp_wire = int_cntr_reg < cfg_data;
-  assign int_tvalid_wire = ( ~int_enbl_reg | s_axis_tvalid ) & int_rst_reg;
-
-  generate
-    begin : STOP
-      always @*
-      begin
-        int_cntr_next = int_cntr_reg;
-        int_enbl_next = int_enbl_reg;
-
-        if(~int_enbl_reg & int_comp_wire)
-        begin
-          int_enbl_next = 1'b0;
-        end
-
-        if(m_axis_tready & int_tvalid_wire & int_comp_wire)
-        begin
-          int_cntr_next = int_cntr_reg + 1'b1;
-        end
-
-        if(m_axis_tready & int_tvalid_wire & ~int_comp_wire)
-        begin
-          int_enbl_next = 1'b1;
-        end
-      end
+ 
+  always @*
+  begin
+    int_enbl_next = int_enbl_reg;
+    if( ~int_enbl_reg & int_comp_wire)
+    begin
+       int_enbl_next = 1'b1;
     end
-  endgenerate
+  end
 
-  assign s_axis_tready = int_enbl_reg & m_axis_tready;
-  assign m_axis_tdata = s_axis_tdata;
-  assign m_axis_tvalid = int_tvalid_wire;
+  assign m_axis_fifo_tvalid = s_axis_tvalid;
+  assign s_axis_tready = m_axis_fifo_tready;
+  assign m_axis_tvalid = int_enbl_reg ? s_axis_fifo_tvalid : s_axis_tvalid;
+  assign s_axis_fifo_tready = int_enbl_reg ? m_axis_tready : 1'b0;
+  assign m_axis_fifo_tdata=s_axis_tdata;
+  assign m_axis_tdata=s_axis_fifo_tdata;
+
 endmodule
--- cores/axis_delay_v1_0/core_config.tcl
+++ cores/axis_delay_v1_0/core_config.tcl
@@ -6,7 +6,7 @@ set_property DISPLAY_NAME $display_name $core
 set_property DESCRIPTION $display_name $core
 
 core_parameter AXIS_TDATA_WIDTH {AXIS TDATA WIDTH} {Width of the M_AXIS and S_AXIS data buses.}
-core_parameter CNTR_WIDTH {CNTR WIDTH} {Width of the counter register.}
+core_parameter CNTR_WIDTH {CNTR WIDTH} {Width of the data counter.}
 
 set bus [ipx::get_bus_interfaces -of_objects $core m_axis]
 set_property NAME M_AXIS $bus
@@ -16,6 +16,15 @@ set bus [ipx::get_bus_interfaces -of_objects $core s_axis]
 set_property NAME S_AXIS $bus
 set_property INTERFACE_MODE slave $bus
 
+set bus [ipx::get_bus_interfaces -of_objects $core m_axis_fifo]
+set_property NAME M_AXIS_FIFO $bus
+set_property INTERFACE_MODE master $bus
+
+set bus [ipx::get_bus_interfaces -of_objects $core s_axis_fifo]
+set_property NAME S_AXIS_FIFO $bus
+set_property INTERFACE_MODE slave $bus
+
+
 set bus [ipx::get_bus_interfaces aclk]
 set parameter [ipx::get_bus_parameters -of_objects $bus ASSOCIATED_BUSIF]
-set_property VALUE M_AXIS:S_AXIS $parameter
+set_property VALUE M_AXIS:S_AXIS:M_AXIS_FIFO:S_AXIS_FIFO $parameter
