4ff09fcc05586b86464191fcc595eb924e6a1787
Fedor Balakirev <fedor@lanl.gov>
2024-01-12 10:07:17 -0700

    Size change

--- projects/ld_test/block_design.tcl
+++ projects/ld_test/block_design.tcl
@@ -57,7 +57,7 @@ cell pavel-demin:user:axis_gpio_reader gpio_0 {
 
 # Create axi_hub
 cell pavel-demin:user:axi_hub hub_0 {
-  CFG_DATA_WIDTH 416
+  CFG_DATA_WIDTH 448
   STS_DATA_WIDTH 288
 } {
   S_AXI ps_0/M_AXI_GP0
@@ -79,7 +79,7 @@ cell pavel-demin:user:axis_red_pitaya_adc adc_0 {
 
 # Create xlslice
 cell pavel-demin:user:port_slicer slice_frequency {
-  DIN_WIDTH 416 DIN_FROM 95 DIN_TO 64
+  DIN_WIDTH 448 DIN_FROM 95 DIN_TO 64
 } {
   Din hub_0/cfg_data
 }
@@ -140,28 +140,28 @@ cell pavel-demin:user:axis_red_pitaya_dac dac_0 {
 
 # Create port_slicer
 cell pavel-demin:user:port_slicer writer_reset_slice {
-  DIN_WIDTH 416 DIN_FROM 0 DIN_TO 0
+  DIN_WIDTH 448 DIN_FROM 0 DIN_TO 0
 } {
   din hub_0/cfg_data
 }
 
 # Create port_slicer
 cell pavel-demin:user:port_slicer run_slice {
-  DIN_WIDTH 416 DIN_FROM 1 DIN_TO 1
+  DIN_WIDTH 448 DIN_FROM 1 DIN_TO 1
 } {
   din hub_0/cfg_data
 }
 
 # Create xlslice
 cell pavel-demin:user:port_slicer slice_trig_record {
-  DIN_WIDTH 416 DIN_FROM 3 DIN_TO 3
+  DIN_WIDTH 448 DIN_FROM 3 DIN_TO 3
 } {
    din hub_0/cfg_data
 }
 
 # Create port_slicer
 #cell pavel-demin:user:port_slicer trig_polarity_slice {
-#  DIN_WIDTH 416 DIN_FROM 16 DIN_TO 16
+#  DIN_WIDTH 448 DIN_FROM 16 DIN_TO 16
 #} {
 #  din hub_0/cfg_data
 #}
@@ -173,14 +173,14 @@ cell xilinx.com:ip:xlconstant trig_polarity_slice {
 
 # Create port_slicer
 cell pavel-demin:user:port_slicer writer_address_slice {
-  DIN_WIDTH 416 DIN_FROM 415 DIN_TO 384
+  DIN_WIDTH 448 DIN_FROM 415 DIN_TO 384
 } {
   din hub_0/cfg_data
 }
 
 # Create port_slicer
 #cell pavel-demin:user:port_slicer trig_mask_slice {
-#  DIN_WIDTH 416 DIN_FROM 383 DIN_TO 368
+#  DIN_WIDTH 448 DIN_FROM 383 DIN_TO 368
 #} {
 #  din hub_0/cfg_data
 #}
@@ -193,7 +193,7 @@ cell xilinx.com:ip:xlconstant trig_mask_slice {
 
 # Create port_slicer
 #cell pavel-demin:user:port_slicer trig_level_slice {
-#  DIN_WIDTH 416 DIN_FROM 367 DIN_TO 352
+#  DIN_WIDTH 448 DIN_FROM 367 DIN_TO 352
 #} {
 #  din hub_0/cfg_data
 #}
@@ -204,21 +204,17 @@ cell xilinx.com:ip:xlconstant trig_level_slice {
 }
 
 # Create port_slicer
-#cell pavel-demin:user:port_slicer pre_data_slice {
-#  DIN_WIDTH 416 DIN_FROM 127 DIN_TO 96
-#} {
-#  din hub_0/cfg_data
-#}
-
-# Create xlconstant
-cell xilinx.com:ip:xlconstant pre_data_slice {
-  CONST_WIDTH 32
-  CONST_VAL 512
+cell pavel-demin:user:port_slicer pre_data_slice {
+  DIN_WIDTH 448 DIN_FROM 447 DIN_TO 416
+} {
+  din hub_0/cfg_data
 }
+
+
 
 # Create port_slicer
 cell pavel-demin:user:port_slicer tot_data_slice {
-  DIN_WIDTH 416 DIN_FROM 63 DIN_TO 32
+  DIN_WIDTH 448 DIN_FROM 63 DIN_TO 32
 } {
   din hub_0/cfg_data
 }
@@ -251,6 +247,12 @@ cell pavel-demin:user:axis_oscilloscope scope_0 {
   aresetn writer_reset_slice/dout
 }
 
+# Create xlconstant
+cell xilinx.com:ip:xlconstant buffer_size {
+  CONST_WIDTH 32
+  CONST_VAL 2097151
+}
+
 # Create axis_ram_writer
 cell pavel-demin:user:axis_ram_writer writer_0 {
   ADDR_WIDTH 18
@@ -260,7 +262,8 @@ cell pavel-demin:user:axis_ram_writer writer_0 {
 } {
   S_AXIS scope_0/M_AXIS
   M_AXI ps_0/S_AXI_ACP
-  cfg_data writer_address_slice/dout
+  cfg_data buffer_size/dout
+  min_addr writer_address_slice/dout
   aclk pll_0/clk_out1
   aresetn writer_reset_slice/dout
 }
