fa4e95d389ea65fad7d117ec51db6418e4fcb698
Fedor Balakirev <fedor@lanl.gov>
2024-03-14 13:59:18 -0600

    Start counter on run flag. Insert zero value at the trigger event to mark the trigger in the stream

--- cores/axis_inf_counter.v
+++ cores/axis_inf_counter.v
@@ -11,6 +11,8 @@ module axis_inf_counter #
   input  wire                        aclk,
   input  wire                        aresetn,
 
+  input  wire                        run_flag,
+  input  wire                        trg_flag,
 
   // Master side
   output wire [AXIS_TDATA_WIDTH-1:0] m_axis_tdata,
@@ -19,42 +21,57 @@ module axis_inf_counter #
 );
 
   reg [CNTR_WIDTH-1:0] int_cntr_reg, int_cntr_next;
-  reg int_enbl_reg, int_enbl_next;
-
+  reg int_run_reg, int_run_next;
+  reg int_trg_reg, int_trg_next;
 
   always @(posedge aclk)
   begin
     if(~aresetn)
     begin
       int_cntr_reg <= {(CNTR_WIDTH){1'b0}};
-      int_enbl_reg <= 1'b0;
+      int_run_reg <= 1'b0;
+      int_trg_reg <= 1'b0;
     end
     else
     begin
       int_cntr_reg <= int_cntr_next;
-      int_enbl_reg <= int_enbl_next;
+      int_run_reg <= int_run_next;
+      int_trg_reg <= int_trg_next;
     end
   end
 
-    begin 
-      always @*
-      begin
-        int_cntr_next = int_cntr_reg;
-        int_enbl_next = int_enbl_reg;
+  begin 
+     always @*
+     begin
+       int_cntr_next = int_cntr_reg;
+       int_run_next = int_run_reg;
+       int_trg_next = int_trg_reg;
 
-        if(~int_enbl_reg )
-        begin
-          int_enbl_next = 1'b1;
-        end
+       if(~int_run_reg & run_flag)
+       begin
+          int_run_next = 1'b1;
+          int_trg_next = 1'b0;
+       end
 
-        if( int_enbl_reg )
-        begin
+       if(int_run_reg & trg_flag)
+       begin
+          int_trg_next = 1'b1;
+       end
+
+       if( int_run_reg )
+       begin
           int_cntr_next = int_cntr_reg + 1'b1;
-        end
-      end
-    end
+       end
 
-  assign m_axis_tdata = int_cntr_reg;
-  assign m_axis_tvalid = int_enbl_reg;
+       if(int_trg_reg)
+       begin       
+          int_trg_next = 1'b0;
+       end
+
+     end
+  end
+
+  assign m_axis_tdata = int_trg_reg?{(CNTR_WIDTH){1'b0}}:int_cntr_reg;
+  assign m_axis_tvalid = 1'b1;
 
 endmodule
