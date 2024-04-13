8adf1dd01575ae544b98feeb919fcef6daed30fe
Fedor Balakirev <fedor@lanl.gov>
2024-04-13 16:50:54 -0600

    Functioning 4adc read, removed memcpy

--- projects/4adc_test/4adc_test.c
+++ projects/4adc_test/4adc_test.c
@@ -7,21 +7,31 @@
 #include <sys/mman.h>
 #include <sys/ioctl.h>
 
-int main()
+int main(int argc, char *argv[])
 {
   int fd, i;
-  int samples=4096;
+  int samples=32768,taps=0;
   volatile uint8_t *rst;
   volatile void *cfg, *sts, *fifo;
   int16_t *buffer;
   int yes = 1;
-  uint32_t read_count,write_count;
+  uint32_t read_count,write_count,data;
 
-  if((buffer = (int16_t *)malloc(65536)) == NULL)
-  {
-    perror("malloc");
-    return EXIT_FAILURE;
+  if (argc >=2 ) {
+	uint32_t tmp = atoi(argv[1]);
+	if (tmp > 0 ) taps=tmp;
   }
+
+  if (argc >=3 ) {
+	uint32_t tmp = atoi(argv[2]);
+	if (tmp > 0 ) samples=tmp;
+  }
+
+  //if((buffer = (int16_t *)malloc(samples*8)) == NULL)
+  //{
+  //  perror("malloc");
+  //  return EXIT_FAILURE;
+  //}
 
   if((fd = open("/dev/mem", O_RDWR)) < 0)
   {
@@ -44,7 +54,21 @@ int main()
   *rst |= 2;
   usleep(2);
   *rst &= ~2;
-  usleep(10);
+  usleep(1000);
+
+  if(taps>0) 
+  {
+      for(i = 0; i < taps; ++i)
+      {
+        *(uint32_t *)fifo =  (uint32_t)0xFFFFFFFF;
+      }
+      
+   }
+
+  usleep(10000);
+
+
+
 
   read_count =  *(uint32_t *)(sts + 8);
   // printf("%x\n ",read_count);
@@ -67,12 +91,15 @@ int main()
 
   // printf("%5d %5d\n",read_count,write_count);  
 
-  memcpy(buffer, (const void *) fifo, samples*8);
+  // memcpy(buffer, (const void *) fifo, samples*8);
   // print samples
   for(i = 0; i < samples; ++i)
   {
-    printf("%5d\t%5d\t%5d\t%5d\t%5d\n",i, buffer[i*4], buffer[i*4+1],buffer[i*4+2], buffer[i*4+3]);
-  }
+    data =  *(uint32_t *)fifo;
+    printf("%5d\t%5d\t%5d",i, data&0xffff, (data>>16)&0xffff);
+    data =  *(uint32_t *)fifo;
+    printf("\t%5d\t%5d\n", data&0xffff, (data>>16)&0xffff);
+   }
 
   return EXIT_SUCCESS;
 }
