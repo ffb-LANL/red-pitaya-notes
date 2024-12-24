c87ba2674cc981e35156275c89078092ee7469d7
Fedor Balakirev <fedor@lanl.gov>
2024-12-24 14:17:15 -0700

    Adjusted scaling math

--- cores/axis_scaler.v
+++ cores/axis_scaler.v
@@ -10,7 +10,7 @@ module axis_scaler #
   input  wire                        aclk,
   input  wire                        aresetn,
 
-  input  wire signed  [AXIS_TDATA_WIDTH*2-1:0]   cfg_data,
+  input  wire signed  [31:0]   cfg_data,
 
     // Slave side
   input wire signed [AXIS_TDATA_WIDTH-1:0] s_axis_tdata,
@@ -27,15 +27,20 @@ module axis_scaler #
 
 reg signed [AXIS_TDATA_WIDTH-1:0] s_axis_tdata_reg,s_axis_tdata_next;
 
-reg [AXIS_TDATA_WIDTH*2-1:0] int_data_reg, int_data_next;
+reg [AXIS_TDATA_WIDTH+15:0] int_data_reg, int_data_next;
 
-wire signed [AXIS_TDATA_WIDTH-1:0] scale, offset;
+wire signed [15:0] scale;
+wire signed [AXIS_TDATA_WIDTH-1:0] offset;
 
-assign scale = cfg_data[AXIS_TDATA_WIDTH-1:0];
+wire signed  [AXIS_TDATA_WIDTH+15:0] mult_result;
 
-assign offset = cfg_data[AXIS_TDATA_WIDTH*2-1:AXIS_TDATA_WIDTH];
+assign scale = cfg_data[15:0];
+
+assign offset = cfg_data[AXIS_TDATA_WIDTH+15:16];
 
 wire multiply = s_axis_tvalid & m_axis_tready;
+
+assign mult_result = s_axis_tdata*scale;
 
 always @(posedge aclk)
 	begin
@@ -50,10 +55,10 @@ always @(posedge aclk)
     			begin
      				if(multiply)
 					begin
-						s_axis_tdata_reg <= s_axis_tdata;
-						s_axis_tdata_next <= s_axis_tdata_reg;
- 						int_data_reg <= (s_axis_tdata_next-offset)*scale;
-        					int_data_next <= int_data_reg; 
+						s_axis_tdata_reg <= mult_result[AXIS_TDATA_WIDTH+14:15]+offset;
+						// s_axis_tdata_next <= s_axis_tdata_reg;
+ 						// int_data_reg <= (s_axis_tdata_next-offset)*scale;
+        					// int_data_next <= int_data_reg; 
     					end
 			end
 	end
@@ -62,6 +67,6 @@ assign m_axis_tvalid = s_axis_tvalid;
 
 //scales down relative to 2^(AXIS_TDATA_WIDTH-2), e.g. cfg=4096  for 14 bit equals scale of 1
 
-assign m_axis_tdata = int_data_next[AXIS_TDATA_WIDTH*2-3:AXIS_TDATA_WIDTH-2];
+assign m_axis_tdata = s_axis_tdata_reg; //int_data_next[AXIS_TDATA_WIDTH+15:16];
 
 endmodule
