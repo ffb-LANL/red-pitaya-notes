a62cc332c6f681301aa75098315f14b23b6e7b23
Fedor Balakirev <fedor@lanl.gov>
2025-01-29 15:24:24 -0700

    Scale with offset DSP inference

--- cores/axis_scaler.v
+++ cores/axis_scaler.v
@@ -3,14 +3,15 @@
 
 module axis_scaler #
 (
-  parameter integer AXIS_TDATA_WIDTH = 14
+  parameter integer AXIS_TDATA_WIDTH = 14,
+  parameter integer DSP_LATENCY = 2
 )
 (
   // System signals
   input  wire                        aclk,
   input  wire                        aresetn,
 
-  input  wire signed  [31:0]   cfg_data,
+  input  wire               [31:0]   cfg_data,
 
     // Slave side
   input wire signed [AXIS_TDATA_WIDTH-1:0] s_axis_tdata,
@@ -24,49 +25,50 @@ module axis_scaler #
 
 );
  
-
-reg signed [AXIS_TDATA_WIDTH-1:0] s_axis_tdata_reg,s_axis_tdata_next;
-
-reg [AXIS_TDATA_WIDTH+15:0] int_data_reg, int_data_next;
-
 wire signed [15:0] scale;
-wire signed [AXIS_TDATA_WIDTH-1:0] offset;
+wire signed [AXIS_TDATA_WIDTH+15:0] offset;
+reg signed  [47:0] result;
+// reg signed [47:0] dsp_result_pipeline [0:DSP_LATENCY-1];
+reg dsp_valid_pipeline [0:DSP_LATENCY-1];
+assign m_axis_tvalid = s_axis_tvalid; // dsp_valid_pipeline[0];
 
-wire signed  [AXIS_TDATA_WIDTH+15:0] mult_result;
+// Handshake logic
+assign s_axis_tready = m_axis_tready; // || !m_axis_tvalid);
+                   
 
-assign scale = cfg_data[15:0];
 
-assign offset = cfg_data[AXIS_TDATA_WIDTH+15:16];
+assign scale = $signed(cfg_data[15:0]);
+assign offset = $signed({cfg_data[AXIS_TDATA_WIDTH+15:AXIS_TDATA_WIDTH+15],cfg_data[AXIS_TDATA_WIDTH+15:16],15'b0});
 
-wire multiply = s_axis_tvalid & m_axis_tready;
-
-assign mult_result = s_axis_tdata*scale;
+integer i;
 
 always @(posedge aclk)
-	begin
-		if(~aresetn)
-      			begin
-				s_axis_tdata_reg <= 0;
-				s_axis_tdata_next <= 0;
-        			int_data_reg <= 0;
-        			int_data_next <= 0;
-      			end
-    		else
-    			begin
-     				if(multiply)
-					begin
-						s_axis_tdata_reg <= mult_result[AXIS_TDATA_WIDTH+14:15]+offset;
-						// s_axis_tdata_next <= s_axis_tdata_reg;
- 						// int_data_reg <= (s_axis_tdata_next-offset)*scale;
-        					// int_data_next <= int_data_reg; 
-    					end
-			end
-	end
-assign s_axis_tready = m_axis_tready;
-assign m_axis_tvalid = s_axis_tvalid;
+    begin
+	if(~aresetn)
+      	  begin
+            result <= 48'b0;
+           // for (i = 0; i < DSP_LATENCY; i = i + 1) begin
+            //    dsp_result_pipeline[i] <= 48'b0;
+             //   dsp_valid_pipeline[i] <= 1'b0;
+           // end
+      	  end
+    	else
+    	  begin
+            if (s_axis_tvalid && s_axis_tready) begin
+                // Start new DSP operation
+                (* use_dsp = "yes" *)
+                result <= $signed(s_axis_tdata) * scale + offset;
+              //  dsp_valid_pipeline[0] <= 1'b1;
+            end
 
-//scales down relative to 2^(AXIS_TDATA_WIDTH-2), e.g. cfg=4096  for 14 bit equals scale of 1
+            // Pipeline the DSP results and valid signals
+           // for (i = 1; i < DSP_LATENCY; i = i + 1) begin
+                // dsp_result_pipeline[i] <= dsp_result_pipeline[i-1];
+               // dsp_valid_pipeline[i] <= dsp_valid_pipeline[i-1];
+            // end
+          end
+     end
 
-assign m_axis_tdata = s_axis_tdata_reg; //int_data_next[AXIS_TDATA_WIDTH+15:16];
+assign m_axis_tdata = $signed(result[AXIS_TDATA_WIDTH+14:15]); 
 
 endmodule
